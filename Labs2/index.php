f212688f4e2b1633fce44d44e38ff16a3be5709e
Lyndee Pablo Garde <lyndee1219@gmail.com>
2026-08-12 10:23:27 +0800

    added pages

---
+++ Labs2/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1>Navigate Pages:</h1>
+        <a href="index.php">Home</a>
+        <a href="about.php">About Us</a>
+        
+    </body>
+</html>
