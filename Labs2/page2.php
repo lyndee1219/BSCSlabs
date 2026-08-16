b49cb8182925e5c0bf98e9df6a51dd265b874a5e
Lyndee Garde <lyndee1219@gmail.com>
2026-08-16 21:38:03 +0800

    Updated labs2 biography

---
+++ Labs2/page2.php
@@ -0,0 +1,134 @@
+<?php
+    $title = "My Hobbies";
+    $header = "MY HOBBIES";
+?>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $title; ?></title>
+    </head>
+
+    <body>
+
+        <h1><?php echo $header; ?></h1>
+
+        <p>
+            Here are some of the hobbies and activities that I enjoy
+            doing during my free time.
+        </p>
+
+        <h2>Things I Like To Do</h2>
+
+        <ul>
+            <li>Drawing</li>
+            <li>Playing video games</li>
+            <li>Watching movies</li>
+            <li>Gloving</li>
+            <li>Manipulating pawns</li>
+            <li>Suppressing my Emotions</li>
+            <li>Punching metal for fun</li>
+        </ul>
+
+        <h2>Drawing</h2>
+
+        <p>
+            I like to draw sometimes I guess..
+        </p>
+
+        <img src="https://pbs.twimg.com/media/GzCfrDQaAAAeevz.jpg"
+             alt="drawing"
+             width="200">
+        <hr>
+        <h2>Video Games</h2>
+
+        <p>
+            I love playing video games
+        </p>
+
+        <img src="https://preview.redd.it/huge-loss-streak-i-want-to-give-up-v0-ba7iew68oguc1.png?width=1033&format=png&auto=webp&s=db28d3f73b58c771cea0406617d3077ee4b0475b"
+             alt="Video games"
+             width="300">
+
+        <hr>
+
+        <h2>Video Games I actually like</h2>
+
+        <table> 
+            <tr>
+                <td><img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTOoFMAtedAWGYWPGX8ZOxF4Ty40YDUh42HeRTFT-7fiqoUS5Ld"
+             alt="Video games"
+             width="300"></td>
+
+             <td><img src="https://upload.wikimedia.org/wikipedia/en/thumb/0/0c/Witcher_3_cover_art.jpg/250px-Witcher_3_cover_art.jpg?utm_source=en.wikipedia.org&utm_campaign=parser&utm_content=thumbnail"
+             alt="Video games"
+             width="200"></td>
+
+             <td><img src="https://preview.redd.it/hey-its-my-3rd-resident-evil-4-remake-fanposter-v0-jkumlyatn6ka1.png?width=640&crop=smart&auto=webp&s=69ac5167923fafe1bfe576d2c09efda6d5416151"
+             alt="Video games"
+             width="250"></td>
+            </tr>
+            <tr>    
+                <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP9-0AmelmmI-MrQc9w8AsGprvqZnFu2bK4igf-5KSU3bKyceD_BRWbTw&s=10"
+             alt="Video games"
+             width="250"></td>
+
+             <td><img src="https://images-na.ssl-images-amazon.com/images/I/51MvQ3BnGUL.jpg"
+             alt="Video games"
+             width="250"></td>
+
+             <td><img src="https://upload.wikimedia.org/wikipedia/en/f/fd/Logo_of_Stardew_Valley.png?utm_source=en.wikipedia.org&utm_campaign=index&utm_content=original"
+             alt="Video games"
+             width="250"></td>
+            </tr>
+        </table>
+
+        <hr>
+
+        <h2>Movies/Shows I like</h2>
+
+        <p>
+            Here are some of the stuff I like to watch:
+        </p>
+
+        <table> 
+            <tr>
+               <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpA9G-jodkRONgAL8C9bjp7wEk3QE74GurgC2F1CcY2XLg9_kdZ6KhQC0&s=10"
+             alt="Hannibal"
+             width="260"></td>
+             
+             <td><img src="https://m.media-amazon.com/images/M/MV5BYWQwMGRhNGEtZTNhMy00MzVjLWJhMjItYjcwMDljMTkyNTg2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
+             alt="TWD"
+             width="220"></td> 
+
+             <td><img src="https://static.wikia.nocookie.net/peaky-blinders/images/0/0a/Peaky_Blinders_4.jpg/revision/latest?cb=20180110141315"
+             alt="Peaky Blinders"
+             width="250"></td>
+            </tr>
+            <tr>
+               <td><img src="https://upload.wikimedia.org/wikipedia/en/4/4c/Deadpool_%26_Wolverine_poster.jpg?utm_source=en.wikipedia.org&utm_campaign=index&utm_content=original"
+             alt="DP Wolverine"
+             width="260"></td>
+             
+             <td><img src="https://m.media-amazon.com/images/M/MV5BMTcxNDYwOTEzMl5BMl5BanBnXkFtZTcwOTA3MzY3NA@@._V1_.jpg"
+             alt="Rodrick Rules"
+             width="220"></td> 
+
+             <td><img src="https://static.wikia.nocookie.net/indie/images/e/ef/Metal_Family_official_Poster.jpg/revision/latest?cb=20241028022053"
+             alt="Metal Family"
+             width="250"></td>
+            </tr>
+        </table>
+        <hr>
+        <h3>Navigation</h3>
+
+        <a href="page1.php">About Me</a>
+        <br>
+
+        <a href="page2.php">My Hobbies</a>
+        <br>
+
+        <a href="page3.php">More</a>
+
+    </body>
+</html>
---
+++ Labs2/page3.php
@@ -0,0 +1,31 @@
+<?php
+    $title = "More About Me";
+    $header = "Why are you here?";
+?>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $title; ?></title>
+    </head>
+
+    <body>
+
+        <h1><?php echo $header; ?></h1>
+
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZLkiCSjLk_9jwNctQFmrt1PxVYfHLLSdnYQq6mmtIqQ&s=10" alt="Background image" width="600">
+
+        <hr>
+
+        <h3>Navigation</h3>
+
+        <a href="page1.php">About Me</a>
+        <br>
+
+        <a href="page2.php">My Hobbies</a>
+        <br>
+        <a href="page3.php">More</a>
+
+
+    </body>
+</html>
